a78bd1a9d8743b64cca1ba9743caf7a1bfc65349
Tyler Matteson <tlandonmatteson@gmail.com>
2018-07-30 15:42:20 -0400

    [WIP] Testing changes - test records and not complete or acurate

--- erpnext/accounts/doctype/indirect_expense/test_indirect_expense.py
+++ erpnext/accounts/doctype/indirect_expense/test_indirect_expense.py
@@ -6,6 +6,8 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
+# test_records = frappe.get_test_records('Indirect Expense')
+
 class TestIndirectExpense(unittest.TestCase):
 	pass
 # test non-default AP account curency and conversion from default currency expense
@@ -14,7 +16,7 @@ class TestIndirectExpense(unittest.TestCase):
 # split cost center with same expense account
 # induce rounding
 
-# update Payment Entry
+# update Payment Entry Discount code to include Indirect Expense
 # update project cost code (inline)
 # update landed cost voucher
 # update campaign costing report
